80b1d90e6ac890f7818f2e8559fd699495bb2785
Shuvo <shuvoorba28@gmail.com>
2023-11-24 03:25:42 +0600

    first commit and basic react and react routes set up completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <button>app.jsx</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h3 className='text-5xl'>Hi, this is error page</h3>
+        </div>
+    );
+};
+
+export default ErrorPage;
