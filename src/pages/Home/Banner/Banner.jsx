19b678cc623cee7c93e2f97d0884814830e75424
Shuvo <shuvoorba28@gmail.com>
2023-11-24 08:58:21 +0600

    banner added

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,25 @@
+
+const Banner = () => {
+    return (
+        <>
+            <div className="flex gap-5">
+                <div className="hero mt-10  rounded-lg max-w-6xl min-h-[500px]" style={{ backgroundImage: 'url(https://i.ibb.co/M8gwS23/19948929-6162910.jpg)' }}>
+                    <div className="hero-overlay bg-opacity-20"></div>
+                    <div className="hero-content text-center text-neutral-content">
+                    </div>
+                </div>
+                <div className="max-w-lg mt-10 p-8 rounded-lg">
+                    <h3>Welcome to Voxly</h3>
+                    <p>Shape the future, voice your opinions, and earn seamlessly with our cutting-edge Voxly integrated with secure and rewarding payment options.</p>
+                    <div className="flex flex-col w-[60%] ">
+                        <div className="grid h-12 bg-[#4D455D] rounded-box place-items-center">content</div>
+                        <div className="divider">OR</div>
+                        <div className="grid h-12 bg-[#4D455D] rounded-box place-items-center">content</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Banner;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,11 @@
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <h2 className='text-3xl'>This is home</h2>
+
         </div>
     );
 };
