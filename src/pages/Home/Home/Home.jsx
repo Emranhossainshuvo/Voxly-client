802fec5a64b923391515492f1c5848aecd408ca1
Shuvo <shuvoorba28@gmail.com>
2023-11-24 18:07:28 +0600

    how it works section completed

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
 import Faq from "../Faq/Faq";
+import HowItWorks from "../HowItWorks/HowItWorks";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <HowItWorks></HowItWorks>
             <Faq></Faq>
             <h2 className='text-3xl'>This is home</h2>
 
