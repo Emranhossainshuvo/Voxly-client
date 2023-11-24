2fbc5fda5861454af6e479c605b7b29f03b1eb83
Shuvo <shuvoorba28@gmail.com>
2023-11-25 04:41:32 +0600

    Testimonials Section upgraded with photo

--- src/pages/Home/Testimonials/Testimonials .jsx
+++ src/pages/Home/Testimonials/Testimonials .jsx
@@ -1,35 +1,34 @@
-import React from 'react';
 
 const Testimonials = () => {
     const testimonialsData = [
         {
             id: 1,
-            avatar: 'url_to_avatar_image_1.jpg',
+            avatar: 'https://i.ibb.co/dkztDtz/pretty-smiling-woman-cafe.jpg',
             testimonial: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         },
         {
             id: 2,
-            avatar: 'url_to_avatar_image_2.jpg',
+            avatar: 'https://i.ibb.co/9bznm4s/portrait-blonde-girl-looking-photographer.jpg',
             testimonial: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         },
         {
             id: 3,
-            avatar: 'url_to_avatar_image_3.jpg',
+            avatar: 'https://i.ibb.co/Xb3pX3L/smiling-young-man-with-crossed-arms-outdoors.jpg',
             testimonial: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         },
         {
             id: 4,
-            avatar: 'url_to_avatar_image_4.jpg',
+            avatar: 'https://i.ibb.co/j8XnwHt/smiling-girl-with-ponytail-colourful-pullover-snowy-day.jpg',
             testimonial: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         },
         {
             id: 5,
-            avatar: 'url_to_avatar_image_5.jpg',
+            avatar: 'https://i.ibb.co/jzjZMGs/beautiful-caucasian-businesswoman-suit.jpg',
             testimonial: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
         },
         {
             id: 6,
-            avatar: 'url_to_avatar_image_6.jpg',
+            avatar: 'https://i.ibb.co/XYPRc82/43608.jpg',
             testimonial: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
         },
     ];
