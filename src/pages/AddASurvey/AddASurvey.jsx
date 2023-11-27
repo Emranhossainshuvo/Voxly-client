1d3ae7ae1d39110417e9effd1c5d45c3bd1da918
Shuvo <shuvoorba28@gmail.com>
2023-11-27 11:36:57 +0600

    photo added in the add survey page

--- src/pages/AddASurvey/AddASurvey.jsx
+++ src/pages/AddASurvey/AddASurvey.jsx
@@ -10,7 +10,8 @@ const SurveyForm = () => {
     const title = form.title.value; 
     const description = form.description.value; 
     const category = form.category.value; 
-    const survey = {title, description, category}; 
+    const photo = form.photo.value; 
+    const survey = {title, description, category, photo}; 
     // console.log(survey); 
       const res = await axiosPublic.post('/surveys', survey)
       console.log(res.data); 
@@ -18,6 +19,16 @@ const SurveyForm = () => {
 
   return (
     <form className="max-w-md mx-auto p-4 bg-gray-100 rounded-md shadow-md" onSubmit={handleSubmit}>
+    <label className="block mb-2">
+        Photo:
+        <input
+          type="text"
+          name="photo"
+          className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+         
+        />
+      </label>
+
       <label className="block mb-2">
         Title:
         <input
