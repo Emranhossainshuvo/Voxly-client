c267654463b2fc4353625f88d4e33cce7b6a85de
Shuvo <shuvoorba28@gmail.com>
2023-11-28 11:10:40 +0600

    data loaded for an specific survey using useLoader data

--- src/pages/Surveys/Surveys.jsx
+++ src/pages/Surveys/Surveys.jsx
@@ -51,8 +51,9 @@ const SurveysPage = () => {
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                 {renderDescription(survey.description)}
               </p>
+                
+              <Link to={`/surveysdetails/${survey._id}`}>
               <button>
-              <Link to="/surveysdetails">
               <a
                 href="#"
                 className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -74,8 +75,8 @@ const SurveysPage = () => {
                   />
                 </svg>
               </a>
-              </Link>
               </button>
+              </Link>
             </div>
           </div>
         ))}
--- src/pages/SurveysDetailsPage/SurveysDetailsPage.jsx
+++ src/pages/SurveysDetailsPage/SurveysDetailsPage.jsx
@@ -1,26 +1,28 @@
 // import { useEffect, useState } from "react";
 // import { useLoaderData } from "react-router-dom";
 
-import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+// import { useEffect, useState } from "react";
 
 const SurveysDetailsPage = () => {
 
-    const [survers, setSurveys] = useState(); 
-    // const {title, desctiption, photo, _id} = useLoaderData(); 
+    // const [survers, setSurveys] = useState(); 
+    const {title, desctiption, photo, _id} = useLoaderData(); 
 
-    useEffect( () => {
-        fetch('http://localhost:5000/surveys')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data); 
-            setSurveys(data)
-        })
-    }, [])
+    // useEffect( () => {
+    //     fetch('http://localhost:5000/surveys')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data); 
+    //         setSurveys(data)
+    //     })
+    // }, [])
 
 
     return (
         <div>
-            <h2>hi: {survers.length}</h2>
+            <h2>hi: {title}</h2>
             {/* {
                 survers.map(surver => )
             } */}
